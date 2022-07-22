f52093b4a19278924cfcaab6a9de7ad9edaaed7c
acarriedev <acarrie.dev@gmail.com>
2022-07-22 15:34:26 +0100

    APM-3001 Add paas client for testing auth

---
+++ paas_client/oauth_usage_example.py
@@ -0,0 +1,38 @@
+import requests
+import oauth
+
+def authenticate_with_machine_user():
+  # Authenticate with private key (machine user)
+  token, client = oauth.get_authenticated_client_token(
+    client_id='tardis',
+    base_auth_url='https://identity.ptl.api.platform.nhs.uk',
+    # private_key='/path/to/private.key'
+    private_key='/home/karsten/auth/jwtRS512.key')
+  
+  return token, client
+
+def authenticate_with_user_account():
+  # Authenticate with username and password (user)
+  token, client = oauth.get_authenticated_client_token(
+    client_id='paas-test-client',
+    client_secret='',
+    base_auth_url='https://identity.ptl.api.platform.nhs.uk',
+    username='',
+    get_password=lambda: 'password')
+
+  return token, client
+
+
+# Choose method of authentication
+token, client = authenticate_with_machine_user()
+# token, client = authenticate_with_user_account()
+
+# Test with authenticated client
+response_using_client = client.get('https://proxygen.ptl.api.platform.nhs.uk/apis/hello-paas')
+print(response_using_client)
+
+# Test with token directly
+response_using_token = requests.get(
+  'https://proxygen.ptl.api.platform.nhs.uk/apis/hello-paas', 
+  headers={"Authorization": f"Bearer {token['access_token']}"})
+print(response_using_token)
