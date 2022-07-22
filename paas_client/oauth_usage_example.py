b1548c7158e139b65a029059019497fb05e3d72d
Dan Brewerton-Singh <Daniel.brewerton-Singh1@nhs.net>
2022-07-22 16:23:04 +0100

    apm-3001: updated git actions

--- paas_client/oauth_usage_example.py
+++ paas_client/oauth_usage_example.py
@@ -1,13 +1,28 @@
 import requests
 import oauth
+from docopt import docopt
 
-def authenticate_with_machine_user():
+"""
+oauth_usage_example.py
+
+example of sucessfully accessing a auth URL via Proxygen 
+
+Usage:
+  oauth_usage_example.py <private_key> <client_id>  
+  oauth_usage_example.py (-h | --help)
+
+Options:
+  -h --help                        Show this screen.  
+  <private_key>                    Private key
+  <client_id>                      Client ID
+"""
+def authenticate_with_machine_user(private_key, client_id):
   # Authenticate with private key (machine user)
   token, client = oauth.get_authenticated_client_token(
-    client_id='tardis',
+    client_id=client_id,
     base_auth_url='https://identity.ptl.api.platform.nhs.uk',
     # private_key='/path/to/private.key'
-    private_key='/home/karsten/auth/jwtRS512.key')
+    private_key=private_key)
   
   return token, client
 
@@ -22,17 +37,24 @@ def authenticate_with_user_account():
 
   return token, client
 
+def main(args):
+  private_key = args['<private_key>']
+  client_id = args['<client_id>']
 
-# Choose method of authentication
-token, client = authenticate_with_machine_user()
-# token, client = authenticate_with_user_account()
+  token, client = authenticate_with_machine_user(private_key,client_id)
 
-# Test with authenticated client
-response_using_client = client.get('https://proxygen.ptl.api.platform.nhs.uk/apis/hello-paas')
-print(response_using_client)
+  # Choose method of authentication
+  # token, client = authenticate_with_user_account()
 
-# Test with token directly
-response_using_token = requests.get(
-  'https://proxygen.ptl.api.platform.nhs.uk/apis/hello-paas', 
-  headers={"Authorization": f"Bearer {token['access_token']}"})
-print(response_using_token)
+  # Test with authenticated client
+  response_using_client = client.get('https://proxygen.ptl.api.platform.nhs.uk/apis/hello-paas')
+  print(response_using_client)
+
+  # Test with token directly
+  response_using_token = requests.get(
+    'https://proxygen.ptl.api.platform.nhs.uk/apis/hello-paas', 
+    headers={"Authorization": f"Bearer {token['access_token']}"})
+  print(response_using_token)
+
+if __name__ == "__main__":
+    main(docopt(__doc__, version='1'))
