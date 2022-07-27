1437b3f54bf818b0fc658b4e8bf50ec17dc6df65
acarriedev <acarrie.dev@gmail.com>
2022-07-27 10:53:08 +0100

    APM-3001 Test using script

---
+++ deploy-environment.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -z "$PR_NUMBER" ]
+then
+    instance=$ENVIRONMENT
+else
+    instance=$ENVIRONMENT-$PR_NUMBER
+fi
+export INSTANCE=$instance
+
+instance_payload=$( envsubst < deployment.json )
+
+curl -X POST https://proxygen.ptl.api.platform.nhs.uk/apis/hello-world/instances?apply=true \
+-H "Authorization: Bearer ${PROXYGEN_TOKEN}" \
+-H "Content-Type: application/json" \
+-d "$instance_payload"
