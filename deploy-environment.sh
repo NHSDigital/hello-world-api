0f59fda0a4302d80bfcc5bdfc7294f1ce20f3cb9
acarriedev <acarrie.dev@gmail.com>
2022-07-28 16:16:44 +0100

    APM-3001 Handle basepath

--- deploy-environment.sh
+++ deploy-environment.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
+api_name="hello-world"
 
 if [ -z "$PR_NUMBER" ]
 then
     instance=$ENVIRONMENT
+    base_path=$api_name
 else
-    instance=$ENVIRONMENT-$PR_NUMBER
+    instance=$ENVIRONMENT-pr-$PR_NUMBER
+    base_path=$api_name-pr-$PR_NUMBER
 fi
 echo $instance
 export INSTANCE=$instance
+export BASE_PATH=$base_path
 
 instance_payload=$( envsubst < deployment.json )
 
-curl -X POST https://proxygen.ptl.api.platform.nhs.uk/apis/hello-world/instances?apply=true \
+curl -X POST https://proxygen.ptl.api.platform.nhs.uk/apis/$api_name/instances?apply=true \
 -H "Authorization: Bearer ${PROXYGEN_TOKEN}" \
 -H "Content-Type: application/json" \
 -d "$instance_payload"
