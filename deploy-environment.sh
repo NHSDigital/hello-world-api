2eb847c5820b50bf37391a175a38986172ff34af
harryhmc <coulterharry@gmail.com>
2022-11-15 09:56:12 +0000

    APM-3575 adding logging

--- deploy-environment.sh
+++ deploy-environment.sh
@@ -18,13 +18,18 @@ else
     export TITLE="Hello World API - $instance"
 fi
 
-echo $instance
 export INSTANCE=$instance
 export BASE_PATH=$base_path
 export SANDBOX_DOMAIN=$ENVIRONMENT
 
 npm run publish
 envsubst < build/hello-world.json > build/hello-world-tmp.json
+
+echo "Using BASE_PATH $BASE_PATH"
+echo "Using INSTANCE $INSTANCE"
+echo "Using TITLE $TITLE"
+echo "Using ENVIRONMENT $ENVIRONMENT"
+echo "Using SANDBOX_DOMAIN $SANDBOX_DOMAIN"
 
 curl -X PUT "https://proxygen.ptl.api.platform.nhs.uk/apis/$api_name/environments/$ENVIRONMENT/instances/$BASE_PATH" \
     -H "Authorization: $(proxygen get-token)" \
