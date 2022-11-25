5deb968a72b164aa7c7eb753ff44da40be04841f
harryhmc <coulterharry@gmail.com>
2022-11-25 12:44:22 +0000

    APM-3575 not resolving refs before sending to proxygen

--- scripts/compile-spec.sh
+++ scripts/compile-spec.sh
@@ -3,4 +3,4 @@
 export ref='$ref'
 mkdir -p build
 envsubst < specification/hello-world.yaml > build/hello-world-populated.yaml
-node_modules/.bin/speccy resolve build/hello-world-populated.yaml -i | poetry run python scripts/yaml2json.py > build/hello-world-rendered.json
+cat build/hello-world-populated.yaml | poetry run python scripts/yaml2json.py > build/hello-world-rendered.json
