c87a3f94f40da71c0d061f401435062131884975
dominicplatt2-nhs <dominic.platt2@nhs.net>
2022-12-16 16:52:04 +0000

    Fix ordering

--- tests/test_wait_for_status_and_ping.py
+++ tests/test_wait_for_status_and_ping.py
@@ -3,14 +3,14 @@ import pytest
 from os import getenv
 
 
-@pytest.mark.order(1)
+@pytest.mark.run(order=1)
 @pytest.mark.smoketest
 def test_ping(nhsd_apim_proxy_url):
     resp = requests.get(f"{nhsd_apim_proxy_url}/_ping")
     assert resp.status_code == 200
 
 
-@pytest.mark.order(2)
+@pytest.mark.run(order=2)
 @pytest.mark.smoketest
 def test_wait_for_ping(nhsd_apim_proxy_url):
     retries = 0
@@ -32,7 +32,7 @@ def test_wait_for_ping(nhsd_apim_proxy_url):
     assert deployed_commitId == getenv('SOURCE_COMMIT_ID')
 
 
-@pytest.mark.order(3)
+@pytest.mark.run(order=3)
 @pytest.mark.smoketest
 def test_status(nhsd_apim_proxy_url, status_endpoint_auth_headers):
     resp = requests.get(
@@ -42,7 +42,7 @@ def test_status(nhsd_apim_proxy_url, status_endpoint_auth_headers):
     # Make some additional assertions about your status response here!
 
 
-@pytest.mark.order(4)
+@pytest.mark.run(order=4)
 @pytest.mark.smoketest
 def test_wait_for_status(nhsd_apim_proxy_url, status_endpoint_auth_headers):
     retries = 0
