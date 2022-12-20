c867452b8abe959002572d4b561c9d64fed9828a
dominicplatt2-nhs <dominic.platt2@nhs.net>
2022-12-20 10:00:43 +0000

    Work so far

--- tests/test_wait_for_status_and_ping.py
+++ tests/test_wait_for_status_and_ping.py
@@ -4,14 +4,12 @@ from os import getenv
 
 
 @pytest.mark.run(order=1)
-@pytest.mark.smoketest
 def test_ping(nhsd_apim_proxy_url):
     resp = requests.get(f"{nhsd_apim_proxy_url}/_ping")
     assert resp.status_code == 200
 
 
 @pytest.mark.run(order=2)
-@pytest.mark.smoketest
 def test_wait_for_ping(nhsd_apim_proxy_url):
     retries = 0
     resp = requests.get(f"{nhsd_apim_proxy_url}/_ping")
@@ -33,7 +31,6 @@ def test_wait_for_ping(nhsd_apim_proxy_url):
 
 
 @pytest.mark.run(order=3)
-@pytest.mark.smoketest
 def test_status(nhsd_apim_proxy_url, status_endpoint_auth_headers):
     resp = requests.get(
         f"{nhsd_apim_proxy_url}/_status", headers=status_endpoint_auth_headers
@@ -43,7 +40,6 @@ def test_status(nhsd_apim_proxy_url, status_endpoint_auth_headers):
 
 
 @pytest.mark.run(order=4)
-@pytest.mark.smoketest
 def test_wait_for_status(nhsd_apim_proxy_url, status_endpoint_auth_headers):
     retries = 0
     resp = requests.get(f"{nhsd_apim_proxy_url}/_status", headers=status_endpoint_auth_headers)
