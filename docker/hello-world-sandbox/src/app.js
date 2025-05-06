6d59babb99f3fe0a8d41b9fcd0a84176d5ff6b16
Sophie Clayton <sophie.clayton12@nhs.net>
2025-05-06 15:03:16 +0100

    APM-6214 mimic apimocker open cors policy

--- docker/hello-world-sandbox/src/app.js
+++ docker/hello-world-sandbox/src/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 app.disable("x-powered-by");
+app.use(cors());
+
 const PORT = process.env.PORT || 9000;
 
 
