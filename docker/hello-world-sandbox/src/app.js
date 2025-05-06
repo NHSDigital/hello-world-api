1d85df96bd5b1e2253a62744d590ee67bdb9ea5c
Sophie Clayton <sophie.clayton12@nhs.net>
2025-05-06 11:00:17 +0100

    APM-6214 replace apimocker with express

--- docker/hello-world-sandbox/src/app.js
+++ docker/hello-world-sandbox/src/app.js
@@ -1,6 +1,25 @@
-var ApiMocker = require('apimocker');
-var options = {};
+const express = require('express');
 
-ApiMocker.createServer(options)
-    .setConfigFile('src/config.json')
-    .start();
+const app = express();
+const PORT = process.env.PORT || 9000;
+
+
+app.get('/hello/world', (req, res) => {
+    res.status(200).json({ "message": "Hello World!" });
+});
+
+app.get('/hello/application', (req, res) => {
+    res.status(200).json({ "message": "Hello Application!" });
+});
+
+app.get('/hello/user', (req, res) => {
+    res.status(200).json({ "message": "Hello User!" });
+});
+
+app.get('/_status', (req, res) => {
+    res.status(200).json({ "status": "pass" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
